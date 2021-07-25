348dcd2914658f5020465e273bded58b2c18d21c
nico-villalonga <nicovillalonga90@gmail.com>
2021-07-25 18:18:08 -0300

    added error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,28 @@
 import { json } from "body-parser";
 import express from "express";
+import "express-async-errors";
 
+import { NotFoundError } from "./errors/not-found";
+import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRoute } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRoute } from "./routes/signout";
+import { signupRoute } from "./routes/signup";
 
 const PORT = 3000;
 const app = express();
 app.use(json());
 
 app.use(currentUserRoute);
+app.use(signinRouter);
+app.use(signoutRoute);
+app.use(signupRoute);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,34 @@
+import { Router, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+import { DatabaseConnectionError } from "../errors/database-connection";
+import { RequestValidationError } from "../errors/request-validation";
+
+const router = Router();
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be provided"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    // const { email, password } = req.body;
+
+    console.log("creating user...");
+    throw new DatabaseConnectionError();
+
+    res.send({});
+  }
+);
+
+export { router as signupRoute };
