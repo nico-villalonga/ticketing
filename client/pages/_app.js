b768728f8a611c2f0489af26d03ea1fbc0e852ef
nico-villalonga <nicovillalonga90@gmail.com>
2021-07-29 01:35:46 -0300

    added signin page / handle AppComponent and child getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,27 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-const LoadComponent = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+import buildClient from "../api/build-client";
+
+const AppComponent = ({ Component, pageProps, currentUser }) => (
+  <div>
+    <h1>Header. {currentUser.email}</h1>
+    <Component {...pageProps} />
+  </div>
 );
 
-export default LoadComponent;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx.req);
+  const { data } = await client.get("/api/users/current-user");
+  let pageProps = {};
+
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return{
+    pageProps,
+    ...data,
+  };
+}
+
+export default AppComponent;
--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -7,7 +7,7 @@ const SignupPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { doRequest, errors } = useRequest({
-    url: "/api/users/signup",
+    url: "/api/users/signin",
     method: "post",
     body: { email, password },
     onSuccess: () => Router.push("/"),
@@ -20,7 +20,7 @@ const SignupPage = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h1>Sign Up</h1>
+      <h1>Sign In</h1>
       <div className="form-group">
         <label>Email Address</label>
         <input
@@ -42,7 +42,7 @@ const SignupPage = () => {
 
       {errors}
 
-      <button className="btn btn-primary">Sign Up</button>
+      <button className="btn btn-primary">Sign In</button>
     </form>
   );
 };
