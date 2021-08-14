4a446e75f4b8e1daeae211addf91d26ad48e9621
nico-villalonga <nicovillalonga90@gmail.com>
2021-08-14 13:29:29 -0300

    added nats wrapper connection

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const PORT = 3000;
 
@@ -14,6 +15,16 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect("ticketing", "asd", "http://nats-srv:4222");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed.");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
