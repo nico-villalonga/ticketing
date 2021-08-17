827eba88b63c18c11186ab8d6672b3bacdf430d5
nico-villalonga <nicovillalonga90@gmail.com>
2021-08-16 22:48:09 -0300

    publish event on ticket created/updated

--- tickets/src/routes/create.ts
+++ tickets/src/routes/create.ts
@@ -2,7 +2,9 @@ import { requireAuth, validateRequest } from "@tickex/common";
 import { Request, Response, Router } from "express";
 import { body } from "express-validator";
 
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
 import { Ticket } from "../models/ticket";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = Router();
 
@@ -26,6 +28,13 @@ router.post(
     });
 
     await ticket.save();
+
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,7 +7,9 @@ import {
 import { Request, Response, Router } from "express";
 import { body } from "express-validator";
 
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
 import { Ticket } from "../models/ticket";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = Router();
 
@@ -36,6 +38,13 @@ router.put(
 
     ticket.set({ title, price });
     await ticket.save();
+
+    await new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
